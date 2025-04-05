8a75b8dad6594e84a275d266a6a7606f71b32efd
Chon John <23521688@gm.uit.edu.vn>
2025-04-05 20:36:00 +0700

    ChonJohn

--- BaiTapLon/Bai001/Bai001.cs
+++ BaiTapLon/Bai001/Bai001.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Bai001
 {
@@ -11,8 +9,48 @@ namespace Bai001
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("");
-            Console.ReadLine();
+            List<PhanSo> danhSach = new List<PhanSo>();
+
+            Console.Write("Nhập số lượng phân số: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nNhập phân số thứ {i + 1}:");
+                PhanSo ps = new PhanSo();
+                ps.Nhap();
+                danhSach.Add(ps);
+            }
+
+            Console.WriteLine("\nDanh sách phân số đã nhập:");
+            foreach (var ps in danhSach)
+                ps.Xuat();
+
+            // Sắp xếp tăng dần
+            danhSach.Sort();
+            Console.WriteLine("\nDanh sách phân số sau khi sắp xếp tăng dần:");
+            foreach (var ps in danhSach)
+                ps.Xuat();
+
+            // Sắp xếp giảm dần
+            danhSach.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine("\nDanh sách phân số sau khi sắp xếp giảm dần:");
+            foreach (var ps in danhSach)
+                ps.Xuat();
+
+            // Thực hiện các phép toán giữa 2 phân số đầu tiên (nếu có)
+            if (danhSach.Count >= 2)
+            {
+                var a = danhSach[0];
+                var b = danhSach[1];
+
+                Console.WriteLine($"\nTổng {a} + {b} = {PhanSo.Tong(a, b)}");
+                Console.WriteLine($"Hiệu {a} - {b} = {PhanSo.Hieu(a, b)}");
+                Console.WriteLine($"Tích {a} * {b} = {PhanSo.Tich(a, b)}");
+                Console.WriteLine($"Thương {a} / {b} = {PhanSo.Thuong(a, b)}");
+            }
+
+            Console.ReadLine(); // Để giữ console không tắt ngay
         }
     }
 }
---
+++ BaiTapLon/Bai001/CPhanSo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai001
+{
+    internal class PhanSo : IComparable<PhanSo>
+    {
+        public int Tu { get; set; }
+        public int Mau { get; set; }
+
+        public PhanSo(int tu = 0, int mau = 1)
+        {
+            Tu = tu;
+            Mau = mau == 0 ? 1 : mau;
+            RutGon();
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhập tử số: ");
+            Tu = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhập mẫu số: ");
+            Mau = int.Parse(Console.ReadLine());
+
+            if (Mau == 0)
+            {
+                Console.WriteLine("Mẫu số không thể bằng 0. Gán mẫu = 1.");
+                Mau = 1;
+            }
+
+            RutGon();
+        }
+
+        public void Xuat()
+        {
+            if (Mau == 1)
+                Console.WriteLine($"{Tu}");
+            else
+                Console.WriteLine($"{Tu}/{Mau}");
+        }
+
+        public static PhanSo Tong(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.Tu * b.Mau + b.Tu * a.Mau, a.Mau * b.Mau);
+        }
+
+        public static PhanSo Hieu(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.Tu * b.Mau - b.Tu * a.Mau, a.Mau * b.Mau);
+        }
+
+        public static PhanSo Tich(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.Tu * b.Tu, a.Mau * b.Mau);
+        }
+
+        public static PhanSo Thuong(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.Tu * b.Mau, a.Mau * b.Tu);
+        }
+
+        public void RutGon()
+        {
+            int ucln = UCLN(Math.Abs(Tu), Math.Abs(Mau));
+            Tu /= ucln;
+            Mau /= ucln;
+
+            // Đảm bảo mẫu số luôn dương
+            if (Mau < 0)
+            {
+                Tu = -Tu;
+                Mau = -Mau;
+            }
+        }
+
+        private int UCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
+        public int CompareTo(PhanSo other)
+        {
+            double val1 = (double)this.Tu / this.Mau;
+            double val2 = (double)other.Tu / other.Mau;
+            return val1.CompareTo(val2);
+        }
+
+        public override string ToString()
+        {
+            return Mau == 1 ? $"{Tu}" : $"{Tu}/{Mau}";
+        }
+    }
+}
